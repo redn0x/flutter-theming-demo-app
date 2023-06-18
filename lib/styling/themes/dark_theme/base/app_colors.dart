7f2581104bca0c4e4f6bfbbbcd7b012cf0bee862
Alexander Gietema <alexander.gietema@ordina.nl>
2023-06-19 00:22:21 +0200

    Add DarkAppColors

---
+++ lib/styling/themes/dark_theme/base/app_colors.dart
@@ -0,0 +1,13 @@
+// import 'package:demo_theming/styling/base/app_colors.dart';
+// import 'package:flutter/material.dart';
+
+// class DarkAppColors extends AppColors {
+//   @override
+//   final color1 = Colors.blue[900]!;
+
+//   @override
+//   final color2 = Colors.orange[900]!;
+
+//   @override
+//   final color3 = Colors.green[900]!;
+// }

f628b0c78dbfbdc582916c774092ebeb4e600411
Alexander Gietema <alexander.gietema@ordina.nl>
2023-06-21 13:53:24 +0200

    Clear infoCardstyle

--- lib/styling/widgets/info_card_style.dart
+++ lib/styling/widgets/info_card_style.dart
@@ -1,19 +1,45 @@
-import 'package:demo_theming/styling/app_theme_data.dart';
-import 'package:flutter/material.dart';
 
-class InfoCardStyle {
-  InfoCardStyle(this.theme);
 
-  AppThemeData theme;
 
-  Color get color1 => theme.colors.color1;
-  Color get color2 => theme.colors.color2;
-  Color get color3 => theme.colors.color3;
 
-  late final mainTextStyle = TextStyle(
-    fontSize: theme.fontSizes.cardText,
-  );
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:demo_theming/styling/app_theme_data.dart';
+// import 'package:flutter/material.dart';
+
+// class InfoCardStyle {
+//   InfoCardStyle(this.theme);
+
+//   AppThemeData theme;
+
+//   Color get color1 => theme.colors.color1;
+//   Color get color2 => theme.colors.color2;
+//   Color get color3 => theme.colors.color3;
+
+//   late final mainTextStyle = TextStyle(
+//     fontSize: theme.fontSizes.cardText,
+//   );
+// }
 
 
 
